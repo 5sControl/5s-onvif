76a5c81c14557fec46dae2569dd23b9a955970ca
artsiom <artsiomzhuchkou2401@gmail.com>
2023-03-27 17:53:01 +0300

    chore(onvif.js): update console.log message in app.get function

--- onvif.js
+++ onvif.js
@@ -209,7 +209,7 @@ app.get("/video", async function (req, res) {
             videoPath = await getFilePath(time, camera_ip)
         }
 
-        console.log(videoPath, 'videoPath dsadasd')
+        console.log(videoPath, 'videoPath dsadcasdadasd')
         const videoSize = fs.statSync(videoPath).size;
 
         const CHUNK_SIZE = 10 ** 6; // 1MB
