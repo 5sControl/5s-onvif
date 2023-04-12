e4b6162b54c2f15a9c4e695fab85fec3e62f1438
artsiom <artsiomzhuchkou2401@gmail.com>
2023-04-12 16:47:34 +0300

    chore

--- onvif.js
+++ onvif.js
@@ -427,7 +427,7 @@ const getFreeSpace = async() => {
 
 setInterval(async () => {
     const freeSpace = await getFreeSpace();
-    if (freeSpace < 0.1) {
+    if (freeSpace < 0.9) {
         const videos = await getLast100Videos()
         for (video of videos) {
             await removeFile(video.file_name)
