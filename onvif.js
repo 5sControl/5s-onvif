01ab7798243bae2c6f005ee58ba1236bb752983a
artsiom <artsiomzhuchkou2401@gmail.com>
2023-05-22 16:23:30 +0300

    chore: edited storage notification message

--- onvif.js
+++ onvif.js
@@ -424,7 +424,7 @@ app.use('/onvif-http/snapshot', async function (req, res) {
 setInterval(async () => {
     const freeSpace = await getFreeSpace();
     if (freeSpace < 0.2) {
-        io.emit('storage notification', {"message": "Old videos will be deleted"});
+        io.emit('storage notification', {"message": "Less than 20% of hard drive space left. Old videos will be deleted"});
         const videos = await getLast100Videos(db)
         for (video of videos) {
             await removeFile(video.file_name)
