0204b43b4921e599c3e41c42da35c32cd9ebb449
slanx <andraffal@gmail.com>
2023-10-05 11:14:00 +0300

    feat:change django port

--- system-messages.js
+++ system-messages.js
@@ -1,6 +1,6 @@
 const sendSystemMessage = async (IP, messageBody) => {
     try {
-      const res = await fetch(`http://${IP}:80/api/core/system-message/`, {
+      const res = await fetch(`http://${IP}:8000/api/core/system-message/`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
